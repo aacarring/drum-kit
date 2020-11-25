1d99a21a2a990894d8c757452b0793e140e8710b
Alexa Carrington <acarring@wellesley.edu>
2020-11-25 12:59:41 -0500

    Added funtionality in js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return; //stops function from running if no audio file corresponds to key
+    audio.currentTime = 0;
     audio.play();
+    key.classList.add('playing');
+    key.addEventListener('transitionend', function() {
+        key.classList.remove('playing');
+    })
 });
